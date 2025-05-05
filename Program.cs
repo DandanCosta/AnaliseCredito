d63c67556a80e7a778ac106252b52b378468727e
Danilo Costa <dandan_costa2222@outlook.com>
2025-05-05 10:35:22 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿decimal valor,valorParcela, rendaComprovada, ParcelasPermidas;
+int qtdParcelas;
+
+Console.WriteLine("--- Anilise de Credito ---\n");
+
+Console.Write("Digite o valor do emprestimo: ");
+valor = decimal.Parse(Console.ReadLine()!);
+
+Console.Write("Digite a quatidade de parcelas: ");
+qtdParcelas = int.Parse(Console.ReadLine()!);
+
+Console.Write("Digite a renda mensal comprovada: ");
+rendaComprovada = decimal.Parse(Console.ReadLine()!);
+
+valorParcela = valor / qtdParcelas;
+ParcelasPermidas = rendaComprovada / 100 * 30;
+
+Console.WriteLine($"Parcela: {valorParcela:C2}");
+Console.WriteLine($"Parcelas permitidas: {ParcelasPermidas:C2}");
+
+if(valorParcela > ParcelasPermidas){
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Emprestimo não autorizado");
+}
+else{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Emprestimo autorizado");
+
+
+}
+
+Console.ResetColor();
